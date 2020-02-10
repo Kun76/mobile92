2d31dabdc7068b03ccd49a7d09eb724acf2313ac
YangK <1041502459@qq.com>
2020-02-10 17:51:20 +0800

    r全局样式

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入vant文件
 import Vant from 'vant'
 import 'vant/lib/index.css'
 // rem 基准值适配
 import 'amfe-flexible/index.min.js'
+ // 引入全局的自定义样式  因为要覆盖vant的样式
+import '@/assets/css/global.less'
+// 一并注册使用vant
 Vue.use(Vant)
+//
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,11 @@
-<template>
-  <div></div>
+<<template>
+  <div class='container'>Home</div>
 </template>
 
 <script>
-  export default {
+export default {
+  name: 'home-index'
 }
 </script>
 
-<style lang="less" scoped>
-</style>
+<style scoped lang='less'></style>
