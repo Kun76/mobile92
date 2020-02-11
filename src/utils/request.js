163fe99311ad5dd77e4ac298dbd07058fefd4686
YangK <1041502459@qq.com>
2020-02-11 10:51:29 +0800

    配置axios

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,9 +4,9 @@ import store from '@/store' // 引入vuex中的store实例
 // 创建axios实例对象
 const instance = axios.create({
   // 设置请求地址常量
-  baseURL: 'http://ttapi.research.itcast.cn/app/v1_0', 
+  baseURL: 'http://ttapi.research.itcast.cn/app/v1_0',
   //设置大数据转换器
-  transformResponse: [function (data) { 
+  transformResponse: [function (data) {
     try {
       return JSONBig.parse(data)
     } catch (error) {
@@ -14,5 +14,29 @@ const instance = axios.create({
     }
   }]
 })
+// 配置请求拦截器
+instance.interceptors.request.use(function (config) {
+  if (store.state.user.token) {
+    config.headers['Authorization'] = `Bearer ${store.state.user.token}`
+  }
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+//配置响应拦截器
+instance.interceptors.response.use(function (response) {
+  try {
+    return response.data.data
+  } catch (error) {
+    return response.data
+  }
+}, function (error) {
+  if (error.response.status === 401) {
+    router.push('/login')
+    // return一个空函数来防止报错
+    return new Promise(function () { })
+  }
+  return Promise.reject(error);
+});
 //导出
 export default instance
